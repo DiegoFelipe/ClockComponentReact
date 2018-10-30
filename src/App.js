58bd1522da0b183f541b043d1b3ccb0b8dc8567f
Diego Felipe <diegofelipe.14@gmail.com>
2018-10-30 12:21:56 -0400

    Adding Clock Component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Clock from './components/Clock'
+
 class App extends Component {
   render() {
     return (
@@ -11,6 +13,7 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
+          <Clock />
           <a
             className="App-link"
             href="https://reactjs.org"
---
+++ src/components/Clock.jsx
@@ -0,0 +1,32 @@
+import React, { Component, Fragment } from 'react'
+
+export default class Clock extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {date: new Date()}
+    }
+
+    componentWillMount() {
+        this.timerID = setInterval(
+            () => this.tick(), 1000
+        )
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerID)
+    }
+
+    tick() {
+        this.setState({
+            date: new Date()
+        })
+    }
+
+    render() {
+        return (
+            <Fragment>
+                <h2>{this.state.date.toLocaleTimeString()}</h2>
+            </Fragment>
+        )
+    }
+}
