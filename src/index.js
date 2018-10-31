1fa8b8bc04ab002d4570fe4034809b992faa0895
Diego Felipe <diegofelipe.14@gmail.com>
2018-10-30 20:17:45 -0400

    updating component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { Component, Fragment } from 'react';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+export default class Clock extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {date: new Date()}
+    }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    componentWillMount() {
+        this.timerID = setInterval(
+            () => this.tick(), 1000
+        )
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerID)
+    }
+
+    tick() {
+        this.setState({
+            date: new Date()
+        })
+    }
+
+    render() {
+        return (
+            <Fragment>
+                <h2>{this.state.date.toLocaleTimeString()}</h2>
+            </Fragment>
+        )
+    }
+}
